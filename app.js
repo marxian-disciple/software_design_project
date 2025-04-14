77494c47d3913691be0e5b4594761764face80ec
emihle booi <2671438@students.wits.ac.za>
2025-04-14 13:18:29 +0200

    added the endpoints folder & got the sign up with google part working

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(express.urlencoded({extended: true}));
 //Our routes here
 const authRoutes = require('./routes/authRoutes');
 app.use(authRoutes);
+const signupRoutes = require('./endpoints/signup');
+app.use(signupRoutes);
 
 //We export here so that we can test it
 module.exports = app;
