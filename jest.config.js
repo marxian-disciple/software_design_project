1e87a056541c447dd2345373c20bbcb7835b070a
molemo <2703800@students.wits.ac.za>
2025-04-23 13:00:53 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,13 @@ module.exports = {
     "local_artisan/scripts/**/*.js", // adjust path as needed
     "!**/node_modules/**",
     "!**/vendor/**"
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
