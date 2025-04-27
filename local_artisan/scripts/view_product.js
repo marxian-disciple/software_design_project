1f91f73981be57bd8c92cdfef55f9d63bb14d893
mohauMakunyane22 <2664333@students.wits.ac.za>
2025-04-27 16:31:34 +0200

    added view_cart.html and changed file destinations in code

--- local_artisan/scripts/view_product.js
+++ local_artisan/scripts/view_product.js
@@ -4,14 +4,14 @@ const products = [
         name: "Handmade Ceramic Mug",
         price: 25.00,
         description: "This beautifully crafted ceramic mug is perfect for coffee lovers. Handmade by local artisans with eco-friendly materials.",
-        image: "path-to-your-image1.jpg"
+        image: "../images/ceramic_mug.png"
     },
     {
         id: "2",
         name: "Wooden Jewelry Box",
         price: 45.00,
         description: "A finely crafted jewelry box made from sustainable wood, featuring intricate carvings.",
-        image: "path-to-your-image2.jpg"
+        image: "../images/wooden_jewelry_box.png"
     }
 ];
 // Get the product ID from the URL
