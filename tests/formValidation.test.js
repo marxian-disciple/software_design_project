2249fbd354af9d8ad0e7c56660ff05c0efe5a9ed
topi2005 <160849388+topi2005@users.noreply.github.com>
2025-05-05 12:36:03 +0200

    Update formValidation.test.js

--- tests/formValidation.test.js
+++ tests/formValidation.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-const { isEmailValid, isPhoneValid, areRequiredFieldsFilled } = require('./formValidation');
+const { isEmailValid, isPhoneValid, areRequiredFieldsFilled } = require('../scripts/formValidation');
 
 describe('Validation Functions', () => {
   test('Valid email passes', () => {
